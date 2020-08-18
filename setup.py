5d2925e2733237b96304a5bc337cfff9f6577d15
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2020-08-18 16:00:18 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdo',
-      version          =   '1.0.4',
+      version          =   '1.0.6',
       description      =   'Base machinery for performing operations on pftrees',
       long_description =   readme(),
       author           =   'FNNDSC',
