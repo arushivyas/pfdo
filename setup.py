d6aff3b8d38f58ed5c8048e20000d5ee259623fb
Arushi Vyas <arushi@havana.lan>
2020-09-01 13:39:56 -0400

    v1.0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdo',
-      version          =   '1.0.6',
+      version          =   '1.0.8',
       description      =   'Base machinery for performing operations on pftrees',
       long_description =   readme(),
       author           =   'FNNDSC',
